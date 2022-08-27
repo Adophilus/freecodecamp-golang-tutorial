dda0cf197e04d1330a6f2612c51fd969c4822db0
Adophilus <uchenna19of@gmail.com>
2022-08-27 17:31:09 +0100

    still learning channels

--- src/channels/main.go
+++ src/channels/main.go
@@ -1,3 +1,14 @@
 package main
 
 import "fmt"
+
+// channels
+// a basic channel is a 2-way stream
+
+// overview
+// channel basics
+// restricted data flow
+// buffered channels
+// closing channels
+// for...range loops with channels
+// select statements
